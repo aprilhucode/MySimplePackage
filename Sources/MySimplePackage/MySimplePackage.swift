0c4fa538940d4a293b990dfcaf40225889dab37d
April Huang <phuang@beaconfireinc.com>
2024-03-15 11:53:27 -0400

    Add api key validation

--- Sources/MySimplePackage/MySimplePackage.swift
+++ Sources/MySimplePackage/MySimplePackage.swift
@@ -1,8 +1,25 @@
 import Foundation
 
 public struct MySimplePackage {
+    
+    private static let apiKeys = ["private_key_1", "private_key_2"]
+    private static var isApiKeyValid = false
+    
+    // configure key
+    public static func configure(key: String) {
+        isApiKeyValid = apiKeys.contains(key)
+    }
+
+    private static func validateApiKeys() -> Bool {
+        if !isApiKeyValid {
+            print("Please provide a valid API key")
+        }
+        return true
+    }
 
     public static func logTime() {
+//        guard if key belongs to apiKeys return
+        guard validateApiKeys() else { return }
         print("MySimplePackage: The time is \(Date())")
     }
 }
